4d8a22dff7a8cc14c601a325296e6ef9792e096a
Graziele Oliveira <graziele.oliveira@sanar.com>
2023-01-08 18:59:04 -0300

    refactor: update styles

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,27 +1,25 @@
 import styled from 'styled-components/native'
 
 export const Container = styled.View`
-    width: 100%;
-    padding: 16px;
-    background-color: white;
-`;
+  width: 100%;
+  padding: 16px;
+  background-color: white;
+`
 
 export const InputContainer = styled.View`
-    border-color: grey;
-    padding-left: 16px;
-    padding-right: 16px;
-    background-color: white;
-    border-radius: 24px;
-    border-width: 1px;
-    flex-direction: row;
-    align-items: center;
-    justify-content: space-between;
+  border-color: grey;
+  padding-left: 16px;
+  padding-right: 16px;
+  background-color: white;
+  border-radius: 24px;
+  border-width: 1px;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
 `
 
 export const Input = styled.TextInput`
-    color: black
-`;
+  color: black;
+`
 
-export const IconTouchable = styled.TouchableOpacity`
-
-`;
+export const IconTouchable = styled.TouchableOpacity``
